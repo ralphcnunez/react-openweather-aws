3d12856c2690bc6f12b8c035090db7a44bc4e6b9
“ralphcnunez” <ralphcnunez@gmail.com>
2023-03-21 17:37:58 -0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,17 @@
+
+// require('dotenv').config()
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cbd00a8ed8msh66d6ad273fe7b0bp1aaf8djsnbbb54e46767e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+    export const GEO_URL_API = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+	export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+    export const API_KEY = process.env.REACT_APP_API_KEY
